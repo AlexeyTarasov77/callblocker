db4f3d6708cfb5743c5bcf89b4cae2e529903015
AlexeyTarasov77 <alexxey2707@gmail.com>
2025-07-24 13:14:40 +0200

    feat: add logger middleware

---
+++ src/logger.middleware.ts
@@ -0,0 +1,10 @@
+import { Request, Response, NextFunction } from 'express';
+
+export function loggerMiddleware(req: Request, res: Response, next: NextFunction) {
+  console.log('%s: Request from %s', new Date().toLocaleString(), req.ip);
+  const startTime = performance.now()
+  next();
+  const processingTime = ((performance.now() - startTime) / 1000)
+  console.log('%s: Request from %s succesfully processed, which took: %d seconds', new Date().toISOString(), req.ip, processingTime)
+}
+
